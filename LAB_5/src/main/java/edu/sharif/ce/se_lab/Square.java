9efcf47d735cf564cd72251ea3b48039a5e78e18
Ali <ali.shirmohammadi2000@gmail.com>
2023-05-06 00:04:11 +0330

    override square method and fix tests

--- LAB_5/src/main/java/edu/sharif/ce/se_lab/Square.java
+++ LAB_5/src/main/java/edu/sharif/ce/se_lab/Square.java
@@ -2,6 +2,18 @@ package edu.sharif.ce.se_lab;
 
 public class Square extends Rectangle {
     public Square(double length) {
-        super(0, 0);
+        super(length, length);
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setLength(length);
+        super.setWidth(length);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+        super.setLength(width);
     }
 }
