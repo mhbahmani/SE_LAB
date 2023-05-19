a7cbdc62aa2ce93c8da97debe82df4b845887273
Ali <ali.shirmohammadi2000@gmail.com>
2023-05-19 18:05:28 +0330

    rename Main Class

--- LAB_7/MiniJava/src/main/java/Application.java
+++ LAB_7/MiniJava/src/main/java/Application.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 
 import parser.Parser;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         Parser parser = new Parser();
         parser.startParse(
                 new Scanner(
                         Objects.requireNonNull(
-                                Main.class.getClassLoader().getResourceAsStream("code"))));
+                                Application.class.getClassLoader().getResourceAsStream("code"))));
     }
 }
