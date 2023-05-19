5543d1ac19d658506095beb765668421e26e1353
Ali <ali.shirmohammadi2000@gmail.com>
2023-05-19 19:12:29 +0330

    extract methods from Parser constructor

--- LAB_7/MiniJava/src/main/java/parser/Parser.java
+++ LAB_7/MiniJava/src/main/java/parser/Parser.java
@@ -1,38 +1,35 @@
 package parser;
 
-import log.Log;
 import code_generator.CodeGenerator;
 import code_generator.CodeGeneratorImpl;
 import code_generator.Memory;
 import errorHandler.ErrorHandler;
+import log.Log;
 import resource_loader.ResourceLoader;
 import scanner.LexicalAnalyzer;
 import scanner.token.Token;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Stack;
 
 public class Parser {
     private final ArrayList<Rule> rules;
     private final Stack<Integer> parsStack;
     private final CodeGenerator codeGenerator;
-    private ParseTable parseTable;
+    private final ParseTable parseTable;
 
     public Parser() {
-        parsStack = new Stack<>();
-        parsStack.push(0);
-        try {
-            ResourceLoader parseTableLoader = new ResourceLoader("parseTable");
-            parseTable = new ParseTable(parseTableLoader.getContent().get(0));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        this.parsStack = new Stack<>();
+        this.parsStack.push(0);
+        this.parseTable = loadParseTable();
+        this.rules = loadRules();
+        this.codeGenerator = new CodeGeneratorImpl(new Memory());
+    }
+
+    private ArrayList<Rule> loadRules() {
+        final ArrayList<Rule> rules;
         rules = new ArrayList<>();
         try {
             ResourceLoader ruleLoader = new ResourceLoader("Rules");
@@ -42,7 +39,16 @@ public class Parser {
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }
-        codeGenerator = new CodeGeneratorImpl(new Memory());
+        return rules;
+    }
+
+    private ParseTable loadParseTable() {
+        try {
+            ResourceLoader parseTableLoader = new ResourceLoader("parseTable");
+            return new ParseTable(parseTableLoader.getContent().get(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void startParse(java.util.Scanner sc) {
